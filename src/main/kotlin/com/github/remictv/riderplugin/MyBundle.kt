bf18a08ce950c62fc1b5b6e4ea521c43abe287fb
GitHub Action <action@github.com>
2025-03-23 19:05:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/remictv/riderplugin/MyBundle.kt
+++ src/main/kotlin/com/github/remictv/riderplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.remictv.riderplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/remictv/riderplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/remictv/riderplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.remictv.riderplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
